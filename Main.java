e5bba547ab059a1adcf6862560e4c15f8d289bcd
PoisonTouch <82893327+PoisonTouch@users.noreply.github.com>
2021-11-16 23:03:25 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         int[] villageResources = {200, 100, 300};
-        int[] stolenRecources = ConditionalComputerGameRightVersion.numberOfResources(villageResources, 121);
+        int[] stolenRecources = ConditionalComputerGameRightVersion.numberOfResources(villageResources, 120);
         for (int r : stolenRecources) {
             System.out.println(r);
         }
